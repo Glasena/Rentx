3434543aee4d805a2c068ae27329f1fbe1598a17
Alison Glasenapp <alisonglasenapp@gmail.com>
2022-06-21 22:29:01 -0300

    Listing Available Cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -35,5 +35,15 @@ class CarsRepositoryInMemory implements ICarsRepository {
         return this.cars.find(car => car.license_plate === licensePlate);
     }
 
+    async findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
+        const all = this.cars.filter(car => {
+            if (car.available === true && ((brand && car.brand === brand) || (category_id && car.category_id === category_id) || (name && car.name === name))) {
+                return car;
+            }
+            return null;
+        })
+        return all;
+    }
+
 }
 export { CarsRepositoryInMemory }
--- src/modules/cars/repositories/in-memory/ICarsRepository.ts
+++ src/modules/cars/repositories/in-memory/ICarsRepository.ts
@@ -5,6 +5,7 @@ interface ICarsRepository {
 
     create(data: ICreateCarDTO): Promise<Car>;
     findByLicensePlate(licensePlate: string): Promise<Car>;
+    findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
 
 }
 
