037d86423ff5fed103c842b4b16af57b513440b0
Alison Glasenapp <alisonglasenapp@gmail.com>
2022-09-24 11:15:42 -0300

    Fixed Car Images Route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -23,6 +23,6 @@ carsRoutes.get("/available", listAvailableCarsController.handle);
 
 carsRoutes.post("/specifications/:id", ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);
 
-carsRoutes.post("/images", ensureAuthenticated, ensureAdmin, upload.array("images"), uploadCarImagesController.handle);
+carsRoutes.post("/images/:id", ensureAuthenticated, ensureAdmin, upload.array("images"), uploadCarImagesController.handle);
 
 export { carsRoutes };
