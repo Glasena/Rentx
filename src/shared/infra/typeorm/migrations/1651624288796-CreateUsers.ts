c962b4faac78124118f543eb8991225a9fa52062
Alison Glasenapp <alisonglasenapp@gmail.com>
2022-09-24 11:31:50 -0300

    Created Rentals Migration

--- src/shared/infra/typeorm/migrations/1651624288796-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1651624288796-CreateUsers.ts
@@ -8,7 +8,8 @@ export class CreateUsers1651624288796 implements MigrationInterface {
                 name: "users",
                 columns: [{
                     name: "id",
-                    type: "uuid"
+                    type: "uuid",
+                    isPrimary: true
                 },
                 {
                     name: "name",
