10f46ecb250cb582acc0b03af985a502c4bddefa
Alison Glasenapp <alisonglasenapp@gmail.com>
2023-01-03 20:35:55 -0300

    Controller de Devolução de Carros

--- src/modules/rentals/useCases/DevolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/DevolutionRental/DevolutionRentalUseCase.ts
@@ -3,13 +3,14 @@ import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     id: string;
     user_id: string;
 }
 
+@injectable()
 class DevolutionRentalUseCase {
     
     constructor(        
@@ -24,8 +25,7 @@ class DevolutionRentalUseCase {
     async execute({ id, user_id}: IRequest): Promise<Rental> {
     
         const rental = await this.rentalsRepository.findById(id);
-        const car = await this.carsRepository.findById(id);
-
+        const car = await this.carsRepository.findById(rental.car_id);
 
         if(!rental) {
             throw new AppError("Rental does not exists !");
--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,14 @@
 import { CreateRentalController } from '@modules/rentals/useCases/CreateRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/DevolutionRental/DevolutionRentalController';
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
 
 export { rentalRoutes }
