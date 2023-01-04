9b9f4f7140c8b7932212b493ad8d7e67e40d3d14
Alison Glasenapp <alisonglasenapp@gmail.com>
2023-01-03 21:42:44 -0300

    Listagem de Aluguéis do Usuário

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -41,6 +41,13 @@ class RentalsRepository implements IRentalsRepository {
         return rental;
     }
 
+    async findByUserId(user_id: string): Promise<Rental[]> {
+        const rentals = this.repository.find({ user_id })
+
+        return rentals;
+    }
+
+
 
 }
 
--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalController } from '@modules/rentals/useCases/CreateRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/DevolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/ListRentalsByUser/ListRentalsByUserController';
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -7,8 +8,9 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
-
+rentalRoutes.get("/user", ensureAuthenticated, listRentalsByUserController.handle);
 export { rentalRoutes }
