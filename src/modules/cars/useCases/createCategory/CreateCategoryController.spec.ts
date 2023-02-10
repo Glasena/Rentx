c63ee32764d5e0557782c81cb093b1d22a7cbdac
Alison Glasenapp <alisonglasenapp@gmail.com>
2023-02-10 16:17:05 -0300

    Refatorando testes

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -34,13 +34,13 @@ describe("Create Category Controller", () => {
             password: 'admin'
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token } = responseToken.body;
 
         const response = await request(app).post("/categories").send({
             name: "Category Supertest",
             description: "Category Supertest Description"
         }).set({
-            Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${refresh_token}`
         });
 
         expect(response.status).toBe(201);
@@ -54,13 +54,13 @@ describe("Create Category Controller", () => {
             password: 'admin'
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token } = responseToken.body;
 
         const response = await request(app).post("/categories").send({
             name: "Category Supertest",
             description: "Category Supertest Description"
         }).set({
-            Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${refresh_token}`
         });
 
         expect(response.status).toBe(400);
